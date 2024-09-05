57a51e098b50b1174dbc9c6440cfee85102b26d8
첸지 <dmswisl1017@gmail.com>
2024-09-05 09:25:25 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -5,9 +5,9 @@ const CONFIG = {
     image: "/totoro.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "security engineer",
     bio: "hello *.*",
-    email: ,
-    linkedin: ,
-    github: ,
+    email: "",
+    linkedin: "",
+    github: "",
     instagram: "onzl.c",
   },
   projects: [
